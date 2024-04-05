ee5c44029cff1b900217b9276d75cb5ade72c2af
dev-fatih-erol <dev.fatih.erol@gmail.com>
2024-04-05 07:43:06 +0300

    Initial commit

---
+++ Buying.IntegrationTests/InstructionsControllerTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+
+namespace Buying.IntegrationTests
+{
+    public class InstructionsControllerTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly WebApplicationFactory<Program> _factory;
+
+        public InstructionsControllerTests(WebApplicationFactory<Program> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task GetActiveInstruction_WhenCalled_ReturnsActiveInstruction()
+        {
+            var client = _factory.CreateClient();
+            var response = await client.GetAsync("/instruction/active");
+            response.EnsureSuccessStatusCode();
+
+            var responseBody = await response.Content.ReadAsStringAsync();
+
+            Assert.NotNull(responseBody);
+        }
+
+        [Fact]
+        public async Task Cancel_WhenCalled_ReturnsNoContent()
+        {
+            var client = _factory.CreateClient();
+
+            var response = await client.DeleteAsync("/instruction/cancel");
+
+            Assert.Equal(HttpStatusCode.NoContent, response.StatusCode);
+        }
+    }
+}
